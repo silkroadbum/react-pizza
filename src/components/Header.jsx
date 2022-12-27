6d50948292b42ace93fd649df94e908d3c878414
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2022-12-27 18:56:23 +0600

    Правка разметки

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,18 @@
 function Header() {
   return (
-    <div class="header">
-      <div class="container">
-        <div class="header__logo">
+    <div className="header">
+      <div className="container">
+        <div className="header__logo">
           <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
           <div>
             <h1>React Pizza</h1>
             <p>самая вкусная пицца во вселенной</p>
           </div>
         </div>
-        <div class="header__cart">
-          <a href="/cart.html" class="button button--cart">
+        <div className="header__cart">
+          <a href="/cart.html" className="button button--cart">
             <span>520 ₽</span>
-            <div class="button__delimiter"></div>
+            <div className="button__delimiter"></div>
             <svg
               width="18"
               height="18"
