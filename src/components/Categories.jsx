5ee07981b7e11230421608964e703fdd3760e27f
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2022-12-28 16:18:48 +0600

    Реализовал отрисовку категорий пицц и смены класса активности при клике на категорию

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
+
 function Categories() {
+  const [activeCategory, setActiveCategory] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  const onClickActiveCategory = (index) => {
+    setActiveCategory(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories &&
+          categories.map((category, index) => (
+            <li
+              onClick={() => onClickActiveCategory(index)}
+              className={activeCategory === index ? 'active' : ''}>
+              {category}
+            </li>
+          ))}
       </ul>
     </div>
   );
