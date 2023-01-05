e2f9fd2fd9140d40f7b4a8a27e9856b404ca79e5
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-01-05 19:14:03 +0600

    Создал компонент страницы одной пиццы и настроил роутинг для перехода на нее

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { addItem, selectCartItemById } from '../../redux/slices/cartSlice';
 
@@ -30,8 +31,10 @@ function PizzaBlock({ id, title, price, imageUrl, types, sizes }) {
 
   return (
     <div className="pizza-block">
-      <img className="pizza-block__image" src={imageUrl} alt="Pizza" width="260" height="260" />
-      <h4 className="pizza-block__title">{title}</h4>
+      <Link to={`/pizza/${id}`}>
+        <img className="pizza-block__image" src={imageUrl} alt="Pizza" width="260" height="260" />
+        <h4 className="pizza-block__title">{title}</h4>
+      </Link>
       <div className="pizza-block__selector">
         <ul>
           {types &&
---
+++ src/pages/FullPizza.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const FullPizza = () => {
+  const [pizza, setPizza] = useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function fetchPizza() {
+      try {
+        const { data } = await axios.get(`https://63ac4a95da81ba97617fdf18.mockapi.io/items/${id}`);
+        setPizza(data);
+      } catch (error) {
+        navigate('/');
+        console.error('Ошибка получения данных о пицце', error);
+      }
+    }
+
+    fetchPizza();
+  }, [id]);
+
+  return (
+    <div className="container">
+      {!pizza ? (
+        <div className="full-pizza__loading">Загрузка...</div>
+      ) : (
+        <div className="full-pizza">
+          <img
+            className="full-pizza__image"
+            src={pizza.imageUrl}
+            width={450}
+            height={450}
+            alt={`Пицца ${pizza.title}`}
+          />
+          <h2 className="full-pizza__title">{pizza.title}</h2>
+          <p className="full-pizza__price">{pizza.price} ₽</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FullPizza;
